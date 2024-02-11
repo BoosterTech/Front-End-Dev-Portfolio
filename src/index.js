b0dc7a0097958b938ebfd3666532ae5400a32d3a
Dariusz Podczasik <boostertech@mail.com>
2024-02-11 02:03:29 +0000

    ThemeProvider added, themes updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalStyles } from "./GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { themes } from "./themes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeProvider theme={themes}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
--- src/themes.js
+++ src/themes.js
@@ -1,8 +1,9 @@
 export const themes = {
   color: {
-    background: "#F2F0F6", //greyinsh
+    background: "#F2F0F6", //grey
     navigationBackground: "#FFFFFF", //white
-    navigationFontColor: "#555556",
+    navigationFontColor: "#6A6A6A", //grey
+    navigationFontHover: "#139DF7",
   },
   breakpoint: {
     mobile: "767px",
