06296023c676319ee4e7616487f589c0287caade
Dariusz Podczasik <boostertech@mail.com>
2024-02-17 11:19:00 +0000

    Updated with Home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 // import "./App.css";
 import { Navigation } from "./common/Navigation";
+import Home from "./features/portfolio/Home";
 
 const App = () => {
-  return <Navigation />;
+  return (
+    <>
+      {" "}
+      <Navigation /> <Home />
+    </>
+  );
 };
 
 export default App;
