652b75c980b2ac6fbee387c8dee5553433d8a721
Dariusz Podczasik <boostertech@mail.com>
2024-03-17 18:08:46 +0000

    fotter added

---
+++ src/features/portfolio/Footer/index.js
@@ -0,0 +1,11 @@
+import { TextContainer, Wrapper } from "./styled";
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      <TextContainer>Copyright © 2024. All rights are reserved</TextContainer>
+    </Wrapper>
+  );
+};
+
+export default Footer;
---
+++ src/features/portfolio/Footer/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background-color: #47494a;
+  color: white;
+  max-width: 100%;
+
+  text-align: center;
+
+  margin: 0;
+`;
+
+export const TextContainer = styled.h2`
+  margin: 0;
+  padding: 10px;
+  font-size: 0.8em;
+`;
