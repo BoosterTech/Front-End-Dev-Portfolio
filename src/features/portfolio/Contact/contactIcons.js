ea4acee28c34e2e464ca2616fb95dd00d937c01b
Dariusz Podczasik <boostertech@mail.com>
2024-03-18 19:45:44 +0000

    contactIcons file added

---
+++ src/features/portfolio/Contact/contactIcons.js
@@ -0,0 +1,20 @@
+import whatsAppIcon from "../../../images/whatsappIcon.png";
+import gitHubIcon from "../../../images/gitHubIcon.png";
+import linkedInIcon from "../../../images/linkedInIcon.png";
+import emailIcon from "../../../images/emailIcon.png"
+
+export const icons = [
+  {
+    iconURL: whatsAppIcon,
+    id: "whatsApp",
+  },
+  { iconURL: gitHubIcon, id: "gitHub" },
+  {
+    iconURL: linkedInIcon,
+    id: "linkedIn",
+  },
+  {
+    iconURL:emailIcon,
+    id: "email",
+  },
+];
