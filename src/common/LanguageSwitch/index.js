864c532eb8cf6521c2b8d6ff60d7948ca728768d
Darek Podczasik <boostertech@mail.com>
2024-05-17 21:39:29 +0100

    handleClick function added to each language icon

--- src/common/LanguageSwitch/index.js
+++ src/common/LanguageSwitch/index.js
@@ -2,14 +2,30 @@ import { Wrapper, Icon, IconsWrapper } from "./styled";
 import PLIcon from "../../images/PolandIcon.png";
 import IRLIcon from "../../images/IrelandIcon.png";
 import ESPIcon from "../../images/SpainIcon.png";
+import { useDispatch } from "react-redux";
+import { setLanguage } from "../../Redux/languageSlice";
 
 export const LanguageSwitch = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = (language) => () => {
+    dispatch(setLanguage(language));
+  };
+
   return (
     <Wrapper>
       <IconsWrapper>
-        <Icon src={IRLIcon} alt="IRLflagIcon" />
-        <Icon src={PLIcon} alt="PLflagIcon" />
-        <Icon src={ESPIcon} alt="ESPflagIcon" />
+        <Icon
+          src={IRLIcon}
+          alt="IRLflagIcon"
+          onClick={handleClick("English")}
+        />
+        <Icon src={PLIcon} alt="PLflagIcon" onClick={handleClick("Polish")} />
+        <Icon
+          src={ESPIcon}
+          alt="ESPflagIcon"
+          onClick={handleClick("Spanish")}
+        />
       </IconsWrapper>
     </Wrapper>
   );
