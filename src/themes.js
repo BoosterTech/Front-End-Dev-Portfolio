8fecd766674a77b635c43a3596c774e0cdcad1ad
Darek Podczasik <boostertech@mail.com>
2024-05-17 20:53:35 +0100

    3 languages added: English, polish & Spanish

--- src/themes.js
+++ src/themes.js
@@ -1,8 +1,8 @@
 export const themes = {
   color: {
-    background: "#F2F0F6", //grey
-    navigationBackground: "#FFFFFF", //white
-    navigationFontColor: "#6A6A6A", //grey
+    background: "#F2F0F6", // grey
+    navigationBackground: "#FFFFFF", // white
+    navigationFontColor: "#6A6A6A", // grey
     navigationFontHover: "#139DF7",
   },
   breakpoint: {
@@ -15,6 +15,28 @@ export const themes = {
       contentHeader: "Front-End React\n Developer",
       headerParagraph:
         "Hi, I'm Dariusz Podczasik. A passionate Front-End React Developer based in Trim, Ireland.",
+      skillsetHeader: "My Skillset Includes",
+      learnNextHeader: "What I'm Learning Next",
+    },
+    about: {
+      aboutMeHeader: "About Me",
+      aboutMeParagraph: `
+        <p>Hi, I'm Derek, a Frontend Developer dedicated to crafting exceptional user interfaces and experiences.</p>
+        <p>My primary focus revolves around leveraging cutting-edge technologies to deliver polished solutions that resonate with users.</p>
+        <p>React serves as the cornerstone of my development approach, facilitating the creation of dynamic, multi-page applications with seamless navigation and optimized performance.</p>
+        <p>Additionally, Styled-Components offers a pragmatic approach to styling, allowing me to streamline the design process and maintain consistency across projects.</p>
+        <p>Furthermore, Redux and Axios enhance the development workflow, promoting code integrity and scalability.</p>
+        <p>This robust stack empowers me to efficiently develop high-performance, responsive web applications that meet the demands of today's digital landscape.</p>
+        <p>Driven by a passion for clean and intuitive design, I ensure that every interface I create not only meets but exceeds user expectations.</p>
+        <p>Best regards,<br />Derek</p>
+      `,
+    },
+    contact: {
+      contactParagraph: "Let's collaborate to bring visions to life!",
+    },
+    footer: {
+      footerParagraph:
+        "Copyright© 2024 Dariusz Podczasik. All rights reserved.",
     },
   },
 
@@ -23,6 +45,58 @@ export const themes = {
       contentHeader: "Front-End React\n Deweloper",
       headerParagraph:
         "Witam, jestem Dariusz Podczasik. Programista Front-Endu w React, mieszkający w Trim w Irlandii.",
+      skillsetHeader: "Moje Umiejętności",
+      learnNextHeader: "Czego Uczę Się Dalej",
+    },
+    about: {
+      aboutMeHeader: "O Mnie",
+      aboutMeParagraph: `
+        <p>Witam, jestem Derek, Frontend Developer dedykowany tworzeniu wyjątkowych interfejsów użytkownika i doświadczeń.</p>
+        <p>Moim głównym celem jest wykorzystanie najnowocześniejszych technologii do dostarczania dopracowanych rozwiązań, które rezonują z użytkownikami.</p>
+        <p>React jest podstawą mojego podejścia do rozwoju, ułatwiając tworzenie dynamicznych, wielostronicowych aplikacji z płynną nawigacją i zoptymalizowaną wydajnością.</p>
+        <p>Dodatkowo, Styled-Components oferuje pragmatyczne podejście do stylizacji, pozwalając mi usprawnić proces projektowania i utrzymać spójność w projektach.</p>
+        <p>Ponadto, Redux i Axios usprawniają proces tworzenia, promując integralność kodu i skalowalność.</p>
+        <p>Ten solidny stos technologiczny pozwala mi efektywnie rozwijać wydajne, responsywne aplikacje internetowe, które spełniają wymagania współczesnego cyfrowego krajobrazu.</p>
+        <p>Kierując się pasją do czystego i intuicyjnego designu, dbam o to, aby każdy interfejs, który tworzę, nie tylko spełniał, ale przewyższał oczekiwania użytkowników.</p>
+        <p>Z poważaniem,<br />Derek</p>
+      `,
+    },
+    contact: {
+      contactParagraph: "Współpracujmy, aby urzeczywistnić wizje!",
+    },
+    footer: {
+      footerParagraph:
+        "Copyright© 2024 Dariusz Podczasik. Wszelkie prawa zastrzeżone.",
+    },
+  },
+
+  Spanish: {
+    home: {
+      contentHeader: "Desarrollador Front-End\n React",
+      headerParagraph:
+        "Hola, soy Dariusz Podczasik. Un apasionado desarrollador Front-End React con sede en Trim, Irlanda.",
+      skillsetHeader: "Mis Habilidades Incluyen",
+      learnNextHeader: "Lo Que Estoy Aprendiendo Siguiente",
+    },
+    about: {
+      aboutMeHeader: "Sobre Mí",
+      aboutMeParagraph: `
+        <p>Hola, soy Derek, un Desarrollador Frontend dedicado a crear interfaces de usuario y experiencias excepcionales.</p>
+        <p>Mi principal enfoque gira en torno a aprovechar las tecnologías más avanzadas para ofrecer soluciones pulidas que resuenen con los usuarios.</p>
+        <p>React es la piedra angular de mi enfoque de desarrollo, facilitando la creación de aplicaciones dinámicas de varias páginas con navegación fluida y rendimiento optimizado.</p>
+        <p>Además, Styled-Components ofrece un enfoque pragmático para el estilo, permitiéndome agilizar el proceso de diseño y mantener la consistencia en los proyectos.</p>
+        <p>Asimismo, Redux y Axios mejoran el flujo de trabajo de desarrollo, promoviendo la integridad y escalabilidad del código.</p>
+        <p>Este robusto stack me permite desarrollar eficientemente aplicaciones web de alto rendimiento y responsivas que cumplen con las demandas del panorama digital actual.</p>
+        <p>Impulsado por una pasión por el diseño limpio e intuitivo, me aseguro de que cada interfaz que creo no solo cumpla, sino que supere las expectativas de los usuarios.</p>
+        <p>Saludos cordiales,<br />Derek</p>
+      `,
+    },
+    contact: {
+      contactParagraph: "¡Colaboremos para dar vida a las visiones!",
+    },
+    footer: {
+      footerParagraph:
+        "Copyright© 2024 Dariusz Podczasik. Todos los derechos reservados.",
     },
   },
 };
