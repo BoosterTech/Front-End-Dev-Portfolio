a92a5681bbf745081cef700f2e13b8e444cc81ac
Darek Podczasik <boostertech@mail.com>
2024-05-17 21:41:05 +0100

    new styles applied to 'aboutMeParagraph'

--- src/themes.js
+++ src/themes.js
@@ -22,13 +22,13 @@ export const themes = {
       aboutMeHeader: "About Me",
       aboutMeParagraph: `
         <p>Hi, I'm Derek, a Frontend Developer dedicated to crafting exceptional user interfaces and experiences.</p>
-        <p>My primary focus revolves around leveraging cutting-edge technologies to deliver polished solutions that resonate with users.</p>
-        <p>React serves as the cornerstone of my development approach, facilitating the creation of dynamic, multi-page applications with seamless navigation and optimized performance.</p>
-        <p>Additionally, Styled-Components offers a pragmatic approach to styling, allowing me to streamline the design process and maintain consistency across projects.</p>
-        <p>Furthermore, Redux and Axios enhance the development workflow, promoting code integrity and scalability.</p>
-        <p>This robust stack empowers me to efficiently develop high-performance, responsive web applications that meet the demands of today's digital landscape.</p>
-        <p>Driven by a passion for clean and intuitive design, I ensure that every interface I create not only meets but exceeds user expectations.</p>
-        <p>Best regards,<br />Derek</p>
+         My primary focus revolves around leveraging cutting-edge technologies to deliver polished solutions that resonate with users.<br/>
+         React serves as the cornerstone of my development approach, facilitating the creation of dynamic, multi-page applications with seamless navigation and optimized performance.<br/>
+         Additionally, Styled-Components offers a pragmatic approach to styling, allowing me to streamline the design process and maintain consistency across projects.<br/>
+         Furthermore, Redux and Axios enhance the development workflow, promoting code integrity and scalability.<br/>
+         This robust stack empowers me to efficiently develop high-performance, responsive web applications that meet the demands of today's digital landscape.<br/>
+         Driven by a passion for clean and intuitive design, I ensure that every interface I create not only meets but exceeds user expectations.
+         <p>Best regards,
       `,
     },
     contact: {
@@ -42,7 +42,7 @@ export const themes = {
 
   Polish: {
     home: {
-      contentHeader: "Front-End React\n Deweloper",
+      contentHeader: "Programista Front-Endu w React",
       headerParagraph:
         "Witam, jestem Dariusz Podczasik. Programista Front-Endu w React, mieszkający w Trim w Irlandii.",
       skillsetHeader: "Moje Umiejętności",
@@ -51,14 +51,15 @@ export const themes = {
     about: {
       aboutMeHeader: "O Mnie",
       aboutMeParagraph: `
-        <p>Witam, jestem Derek, Frontend Developer dedykowany tworzeniu wyjątkowych interfejsów użytkownika i doświadczeń.</p>
-        <p>Moim głównym celem jest wykorzystanie najnowocześniejszych technologii do dostarczania dopracowanych rozwiązań, które rezonują z użytkownikami.</p>
-        <p>React jest podstawą mojego podejścia do rozwoju, ułatwiając tworzenie dynamicznych, wielostronicowych aplikacji z płynną nawigacją i zoptymalizowaną wydajnością.</p>
-        <p>Dodatkowo, Styled-Components oferuje pragmatyczne podejście do stylizacji, pozwalając mi usprawnić proces projektowania i utrzymać spójność w projektach.</p>
-        <p>Ponadto, Redux i Axios usprawniają proces tworzenia, promując integralność kodu i skalowalność.</p>
-        <p>Ten solidny stos technologiczny pozwala mi efektywnie rozwijać wydajne, responsywne aplikacje internetowe, które spełniają wymagania współczesnego cyfrowego krajobrazu.</p>
-        <p>Kierując się pasją do czystego i intuicyjnego designu, dbam o to, aby każdy interfejs, który tworzę, nie tylko spełniał, ale przewyższał oczekiwania użytkowników.</p>
-        <p>Z poważaniem,<br />Derek</p>
+      <p>Witam, jestem Derek, Frontend Developerem, który skupia się na tworzeniu wyjątkowych interfejsów użytkownika i doświadczeń.</p>
+      Moje główne zainteresowania koncentrują się na wykorzystaniu najnowocześniejszych technologii do dostarczania dopracowanych rozwiązań, które rezonują z użytkownikami.<br/>
+      React jest podstawą mojego podejścia do rozwoju, umożliwiając tworzenie dynamicznych, wielostronicowych aplikacji z płynną nawigacją i optymalizowaną wydajnością.<br/>
+      Dodatkowo, Styled-Components oferuje pragmatyczne podejście do stylizacji, pozwalając mi usprawnić proces projektowania i utrzymać spójność w projektach.<br/>
+      Ponadto, Redux i Axios ulepszają przepływ pracy podczas tworzenia kodu, promując integralność kodu i skalowalność.<br/>
+      Ten solidny stos technologiczny umożliwia mi efektywne tworzenie wydajnych, responsywnych aplikacji internetowych, które spełniają wymagania dzisiejszego cyfrowego krajobrazu.<br/>
+      Napędzany pasją do czystego i intuicyjnego projektowania, dbam o to, aby każdy interfejs, który tworzę, nie tylko spełniał, ale przekraczał oczekiwania użytkowników.
+      <p>Z poważaniem,
+        
       `,
     },
     contact: {
@@ -72,7 +73,7 @@ export const themes = {
 
   Spanish: {
     home: {
-      contentHeader: "Desarrollador Front-End\n React",
+      contentHeader: "Desarrollador Front-End React",
       headerParagraph:
         "Hola, soy Dariusz Podczasik. Un apasionado desarrollador Front-End React con sede en Trim, Irlanda.",
       skillsetHeader: "Mis Habilidades Incluyen",
@@ -81,14 +82,14 @@ export const themes = {
     about: {
       aboutMeHeader: "Sobre Mí",
       aboutMeParagraph: `
-        <p>Hola, soy Derek, un Desarrollador Frontend dedicado a crear interfaces de usuario y experiencias excepcionales.</p>
-        <p>Mi principal enfoque gira en torno a aprovechar las tecnologías más avanzadas para ofrecer soluciones pulidas que resuenen con los usuarios.</p>
-        <p>React es la piedra angular de mi enfoque de desarrollo, facilitando la creación de aplicaciones dinámicas de varias páginas con navegación fluida y rendimiento optimizado.</p>
-        <p>Además, Styled-Components ofrece un enfoque pragmático para el estilo, permitiéndome agilizar el proceso de diseño y mantener la consistencia en los proyectos.</p>
-        <p>Asimismo, Redux y Axios mejoran el flujo de trabajo de desarrollo, promoviendo la integridad y escalabilidad del código.</p>
-        <p>Este robusto stack me permite desarrollar eficientemente aplicaciones web de alto rendimiento y responsivas que cumplen con las demandas del panorama digital actual.</p>
-        <p>Impulsado por una pasión por el diseño limpio e intuitivo, me aseguro de que cada interfaz que creo no solo cumpla, sino que supere las expectativas de los usuarios.</p>
-        <p>Saludos cordiales,<br />Derek</p>
+      <p>Hola, soy Derek, un Desarrollador de Frontend dedicado a crear interfaces de usuario y experiencias excepcionales.</p>
+      Mi enfoque principal gira en torno a aprovechar las tecnologías de vanguardia para entregar soluciones pulidas que resuenen con los usuarios.<br/>
+      React sirve como la piedra angular de mi enfoque de desarrollo, facilitando la creación de aplicaciones dinámicas de múltiples páginas con navegación fluida y rendimiento optimizado.<br/>
+      Además, Styled-Components ofrece un enfoque pragmático para el estilo, lo que me permite agilizar el proceso de diseño y mantener la consistencia en los proyectos.<br/>
+      Además, Redux y Axios mejoran el flujo de trabajo de desarrollo, promoviendo la integridad del código y la escalabilidad.<br/>
+      Esta sólida pila me permite desarrollar de manera eficiente aplicaciones web de alto rendimiento y responsivas que satisfacen las demandas del panorama digital de hoy.<br/>
+      Impulsado por una pasión por el diseño limpio e intuitivo, me aseguro de que cada interfaz que creo no solo cumpla, sino que supere las expectativas del usuario.
+      <p>Saludos cordiales,
       `,
     },
     contact: {
