bac53e2c29ce02d331463f67d1f539593e695159
Darek Podczasik <boostertech@mail.com>
2024-05-18 19:55:40 +0100

    'link' property added to each icon element of an array

--- src/features/portfolio/Contact/contactIcons.js
+++ src/features/portfolio/Contact/contactIcons.js
@@ -1,20 +1,23 @@
 import whatsAppIcon from "../../../images/whatsappIcon.png";
 import gitHubIcon from "../../../images/gitHubIcon.png";
 import linkedInIcon from "../../../images/linkedInIcon.png";
-import emailIcon from "../../../images/emailIcon.png"
+import emailIcon from "../../../images/emailIcon.png";
 
 export const icons = [
   {
     iconURL: whatsAppIcon,
     id: "whatsApp",
+    link: "https://wa.me/003530862013944",
   },
-  { iconURL: gitHubIcon, id: "gitHub" },
+  { iconURL: gitHubIcon, id: "gitHub", link: "https://github.com/BoosterTech" },
   {
     iconURL: linkedInIcon,
     id: "linkedIn",
+    link: "https://linkedin.com/in/yourusername",
   },
   {
-    iconURL:emailIcon,
+    iconURL: emailIcon,
     id: "email",
+    link: "mailto:boostertech@mail.com",
   },
 ];
