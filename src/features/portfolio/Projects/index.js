71cbdcbf3bbac042dff139208f45d6e42791906a
Darek Podczasik <boostertech@mail.com>
2024-05-18 23:09:24 +0100

    <Projects/> component describing Projects Section created

--- src/features/portfolio/Projects/index.js
+++ src/features/portfolio/Projects/index.js
@@ -1,10 +1,26 @@
-import { Header, Wrapper } from "./styled";
+import { Header, ProjectIcon, Wrapper } from "./styled";
+import projects from "./projects";
+import Tile from "./Tile";
+import gitHubIcon from "../../../images/gitHubIcon.png";
 
 const Projects = () => {
   return (
     <Wrapper>
-      <Header>PROJECTS</Header>
-      {/* <ProjectImage src={tasksListProjectImage} /> */}
+      <ProjectIcon src={gitHubIcon} alt="GitHub Icon" />
+      <Header>
+        Portfolio
+    
+      </Header>
+      {projects.map((project, index) => (
+        <Tile
+          key={index}
+          title={project.title}
+          description={project.description}
+          imageURL={project.imageURL}
+          GitHubPagesURL={project.GitHubPagesURL}
+          GitHubRepoURL={project.GitHubRepoURL}
+        />
+      ))}
     </Wrapper>
   );
 };
