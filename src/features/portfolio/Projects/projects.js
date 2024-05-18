7e6de6240015501e98538bdea153208bfc96be91
Darek Podczasik <boostertech@mail.com>
2024-05-18 23:08:28 +0100

    projects array created with first Moviebrowser project infos

---
+++ src/features/portfolio/Projects/projects.js
@@ -0,0 +1,17 @@
+import movieBrowserImage from "../../../images/movieBrowserProject.png";
+
+const projects = [
+  {
+    title: `Movie Browser`,
+    description: `&nbsp&nbsp The Movie Browser is an intuitive web application designed to enhance your movie-watching experience
+                  by allowing users to search for movies, cast, and crew members effortlessly.
+                  <p>This four-week project was developed as the final group project during "YouCode" Front-End Development course.</p>
+                  &nbspOur team of three developers collaborated to create a sleek and user-friendly interface that provides detailed information on a wide array of movies and their associated personnel. <br>
+                  The application is designed for optimal performance on different devices and is under constant development and improvement.`,
+    imageURL: `${movieBrowserImage}`,
+    GitHubPagesURL: "https://boostertech.github.io/MovieBrowser/#/movies",
+    GitHubRepoURL: "https://github.com/BoosterTech/MovieBrowser.git",
+  },
+];
+
+export default projects;
