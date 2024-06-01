52fd91d501ff08567ee8df11f198085462170c81
Darek Podczasik <boostertech@mail.com>
2024-06-01 13:51:27 +0100

    <Footer> styles adjusted including different screens

--- src/features/portfolio/Footer/styled.js
+++ src/features/portfolio/Footer/styled.js
@@ -4,14 +4,16 @@ export const Wrapper = styled.div`
   background-color: #47494a;
   color: white;
   max-width: 100%;
-
   text-align: center;
-
   margin: 0;
 `;
 
 export const TextContainer = styled.h2`
   margin: 0;
   padding: 10px;
-  font-size: 0.8em;
+  font-size: 1.1rem;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.lg}) {
+    font-size: 0.7rem;
+  }
 `;
