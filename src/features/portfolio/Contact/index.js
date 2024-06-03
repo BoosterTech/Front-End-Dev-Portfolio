89efe416cf763820b479a568ccc3cc9f1615ed99
Darek Podczasik <boostertech@mail.com>
2024-06-03 17:02:15 +0100

    IntersectionObserver added

--- src/features/portfolio/Contact/index.js
+++ src/features/portfolio/Contact/index.js
@@ -1,16 +1,54 @@
 import { icons } from "./contactIcons";
 import { ContactIconStyled, IconsWrapper, Header, Wrapper } from "./styled";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectLanguage } from "../../../Redux/languageSlice";
 import { useTheme } from "styled-components";
+import { useEffect, useRef } from "react";
+import {
+  selectContactVisibility,
+  setContactVisibility,
+} from "../../../Redux/generalSlice";
 
-const Contact = ({id}) => {
+const Contact = ({ id }) => {
   const language = useSelector(selectLanguage);
   const theme = useTheme();
+  const contactRef = useRef("");
+
+  const isContactVisible = useSelector(selectContactVisibility);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(({ isIntersecting }) => {
+          if (isIntersecting) {
+            console.log("Contact section is visible");
+            dispatch(setContactVisibility(true));
+          } else {
+            console.log("Contact section is not visible");
+            dispatch(setContactVisibility(false));
+          }
+        });
+      },
+      { threshold: 1 }
+    );
+
+    if (contactRef.current) {
+      observer.observe(contactRef.current);
+      console.log("Contact section is being observed ref");
+    }
+
+    return () => {
+      if (contactRef.current) {
+        observer.unobserve(contactRef.current);
+        console.log("Contact section is not being observed ref");
+      }
+    };
+  }, []);
 
   return (
-    <Wrapper id={id}>
+    <Wrapper id={id} ref={contactRef}>
       <Header>{theme[language].contact.contactParagraph}</Header>
       <IconsWrapper>
         {icons.map((icon) => (
