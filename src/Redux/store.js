50ea582444bb02f04f427735e601eddba21eaf07
Darek Podczasik <boostertech@mail.com>
2024-06-03 13:21:01 +0100

    generalReducer added to store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import languageReducer from "./languageSlice";
+import generalReducer from "./generalSlice";
 
 const store = configureStore({
   reducer: {
     language: languageReducer,
+    general: generalReducer,
   },
 });
 
