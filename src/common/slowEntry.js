e34e961733dfee1788403b79dd9692b1a2f72421
Darek Podczasik <boostertech@mail.com>
2024-06-16 13:45:39 +0100

    animation defined

---
+++ src/common/slowEntry.js
@@ -0,0 +1,11 @@
+import { keyframes } from "styled-components";
+
+const slowEntry = keyframes`
+   from{
+        opacity: 0;
+       }
+     to{
+        opacity: 1;
+        }`;
+
+export default slowEntry;
