36ee45c8f5655b576e3f0556fa0b279818c2851f
Dariusz Podczasik <boostertech@mail.com>
2025-01-24 17:30:28 +0000

    feat: update themes with enhanced about sections and refined content
    Revised 'About Me' sections across English, Polish, and Spanish languages for greater detail and personal touch.
    Improved formatting and readability in multilingual content headers and paragraphs.
    Added specific references to modern technologies like Next.js, TypeScript, and Tailwind CSS.
    Enhanced introduction and personal journey narratives for better engagement.

--- src/themes.js
+++ src/themes.js
@@ -20,23 +20,34 @@ export const themes = {
 
   English: {
     home: {
-      contentHeader: "Front-End Developer <React/>",
-      headerParagraph:
-        "Hi, I'm Dariusz Podczasik. A passionate Front-End React Developer based in Trim, Ireland.",
+      contentHeader: "Front-End Developer<React | Next/>",
+      headerParagraph: `
+      Hi, I'm Dariusz Podczasik.
+       A passionate Front-End React Developer based in Trim, Ireland.`,
       skillsetHeader: "My Skillset Includes",
       learnNextHeader: "What I'm Learning Next",
     },
     about: {
       aboutMeHeader: "About Me",
       aboutMeParagraph: `
-        <p>Hi, I'm Derek, a Frontend Developer dedicated to crafting exceptional user interfaces and experiences.</p>
-         My primary focus revolves around leveraging cutting-edge technologies to deliver polished solutions that resonate with users.<br/>
-         React serves as the cornerstone of my development approach, facilitating the creation of dynamic, single-page applications with seamless navigation and optimized performance.<br/>
-         Additionally, Styled-Components offers a pragmatic approach to styling, allowing me to streamline the design process and maintain consistency across projects.<br/>
-         Furthermore, Redux and Axios enhance the development workflow, promoting code integrity and scalability.<br/>
-         This robust stack empowers me to efficiently develop high-performance, responsive web applications that meet the demands of today's digital landscape.<br/>
-         Driven by a passion for clean and intuitive design, I ensure that every interface I create not only meets but exceeds user expectations.
-         <p>Best regards,
+        <p>Hi, I'm Derek,</p> 
+        I've been technology fan since I was a child, and coding has always been a big part of that. I've tried out C++, OpenGL, and MCU programming amongst others because I'm curious and amazed what simple lines of code can do.<br/>
+        At first, coding was just a hobby for me a way to be creative and explore new technologies. But as technology advanced, I decided to turn my hobby into a career. I started learning React in January 2022 and quickly fell in love with it. Now, I'm focused on becoming a professional Developer.<br/>
+        <p>I'm looking for my first job as a Frontend Developer. I want to work with people who share my passion for technology and innovation. I'm excited to join a team where I can continue to learn and grow as a developer.<br/>
+        If you’re a startup company looking to grow, I’d love to join and help you succeed with my hard work and dedication.</p>
+        <p>In addition to frontend development, I've also gained some experience with backend technologies. I'm committed to continuous learning, and my goal is to become skilled in both frontend and backend development. I also want to extend my skills to include Android and iOS development with React.</p>
+        <p>I’m excited about the future and what it holds for me.</p>
+        <p>That’s a bit about me. Now, let’s get back to the main topic...</p>
+        
+        <p>My primary focus is using the latest technologies to create great solutions for users.</p>
+        <p>* React is the main tool I use to build dynamic, single-page applications with smooth navigation and great performance.<br/>
+        * Next.js adds server-side rendering and static site generation for better performance and SEO.<br/>
+        * TypeScript ensures my code is safe and maintainable, making it reliable for complex projects.<br/>
+        * Tailwind CSS helps me quickly style websites with a utility-first approach.<br/>
+        * Styled-Components offers another way to style components<br/>
+        * Redux and Axios help manage data efficiently.</br></p
+        <p>This stack lets me build high-performance, responsive web applications with intuitive design.</p>
+        <p>Best regards,</p>
       `,
     },
     contact: {
@@ -59,16 +70,25 @@ export const themes = {
     about: {
       aboutMeHeader: "O Mnie",
       aboutMeParagraph: `
-      <p>Witam, mam na imię Darek i jestem Frontend Developerem, który skupia się na tworzeniu wyjątkowych interfejsów użytkownika i doświadczeń.</p>
-      Moje główne zainteresowania koncentrują się na wykorzystaniu najnowocześniejszych technologii do dostarczania dopracowanych rozwiązań, które rezonują z użytkownikami.<br/>
-      React jest podstawą mojego podejścia do projektu, umożliwiając tworzenie dynamicznych, jednostronicowych aplikacji z płynną nawigacją i optymalną wydajnością.<br/>
-      Dodatkowo, Styled-Components oferuje pragmatyczne podejście do stylizacji, pozwalając mi usprawnić proces projektowania i utrzymać spójność w projektach.<br/>
-      Ponadto, Redux i Axios ulepszają przepływ pracy podczas tworzenia kodu, promując integralność kodu i skalowalność.<br/>
-      Ten solidny stos technologiczny umożliwia mi efektywne tworzenie wydajnych, responsywnych aplikacji internetowych, które spełniają wymagania dzisiejszego świata cyfrowego.<br/>
-      Napędzany pasją do czystego i intuicyjnego projektowania, dbam o to, aby każdy interfejs, który tworzę, nie tylko spełniał, ale przekraczał oczekiwania użytkowników.
-      <p>Z poważaniem,
-        
-      `,
+      <p>Cześć, jestem Derek,</p>
+      Od dziecka byłem fanem technologii, a programowanie zawsze było jej dużą częścią. Próbowałem swoich sił w C++, OpenGL i programowaniu mikrokontrolerów, ponieważ jestem ciekawy i zafascynowany tym, co potrafią zrobić proste linie kodu.<br/>
+      Na początku programowanie było dla mnie tylko hobby – sposobem na bycie kreatywnym i poznawanie nowych technologii. Jednak w miarę jak technologia się rozwijała, postanowiłem   zamienić swoje hobby w karierę. Zacząłem uczyć się Reacta w styczniu 2022 roku i szybko się w nim zakochałem. Teraz skupiam się na zostaniu profesjonalnym deweloperem.<br/>
+      <p>Szukam swojej pierwszej pracy jako Frontend Developer. Chcę pracować z ludźmi, którzy podzielają moją pasję do technologii i innowacji. Jestem podekscytowany możliwością    dołączenia do zespołu, w którym będę mógł się uczyć i rozwijać jako programista.<br/>
+      Jeśli jesteś startupem, który chce się rozwijać, chętnie dołączę i pomogę osiągnąć sukces moją ciężką pracą i zaangażowaniem.</p>
+      <p>Oprócz programowania frontendowego zdobyłem również trochę doświadczenia w technologiach backendowych. Jestem zaangażowany w ciągłe doskonalenie i moim celem jest zdobycie   umiejętności zarówno w frontendzie, jak i backendzie. Chcę również poszerzyć swoje umiejętności o rozwój aplikacji na Androida i iOS za pomocą Reacta.</p>
+      <p>Jestem podekscytowany przyszłością i tym, co mnie czeka.</p>
+      <p>To tyle o mnie. Teraz wróćmy do głównego tematu...</p>
+      
+      <p>Moim głównym celem jest wykorzystywanie najnowszych technologii do tworzenia świetnych rozwiązań dla użytkowników.</p>
+      <p>* React to główne narzędzie, które wykorzystuję do budowy dynamicznych aplikacji jednostronicowych z płynną nawigacją i świetną wydajnością.<br/>
+      * Next.js dodaje renderowanie po stronie serwera i generowanie statycznych witryn dla lepszej wydajności i SEO.<br/>
+      * TypeScript zapewnia bezpieczeństwo i łatwość utrzymania mojego kodu, co czyni go niezawodnym w przypadku złożonych projektów.<br/>
+      * Tailwind CSS pomaga mi szybko stylizować witryny dzięki podejściu "utility-first".<br/>
+      * Styled-Components oferuje alternatywny sposób stylizacji komponentów.<br/>
+      * Redux i Axios pomagają efektywnie zarządzać danymi.</p>
+      <p>Ten stos technologiczny pozwala mi budować wydajne, responsywne aplikacje internetowe o intuicyjnym designie.</p>
+      <p>Serdeczne pozdrowienia,</p>
+`,
     },
     contact: {
       contactParagraph: "Współpracujmy, aby urzeczywistnić wizje!",
@@ -90,15 +110,25 @@ export const themes = {
     about: {
       aboutMeHeader: "Sobre Mí",
       aboutMeParagraph: `
-      <p>Hola, soy Derek, un Desarrollador de Frontend dedicado a crear interfaces de usuario y experiencias excepcionales.</p>
-      Mi enfoque principal gira en torno a aprovechar las tecnologías de vanguardia para entregar soluciones pulidas que resuenen con los usuarios.<br/>
-      React sirve como la piedra angular de mi enfoque de desarrollo, facilitando la creación de aplicaciones dinámicas de una sola página con navegación fluida y rendimiento optimizado.<br/>
-      Además, Styled-Components ofrece un enfoque pragmático para el estilo, lo que me permite agilizar el proceso de diseño y mantener la consistencia en los proyectos.<br/>
-      Además, Redux y Axios mejoran el flujo de trabajo de desarrollo, promoviendo la integridad del código y la escalabilidad.<br/>
-      Esta sólida pila me permite desarrollar de manera eficiente aplicaciones web de alto rendimiento y responsivas que satisfacen las demandas del panorama digital de hoy.<br/>
-      Impulsado por una pasión por el diseño limpio e intuitivo, me aseguro de que cada interfaz que creo no solo cumpla, sino que supere las expectativas del usuario.
-      <p>Saludos cordiales,
-      `,
+      <p>Hola, soy Derek,</p>
+      Desde niño he sido un fanático de la tecnología, y la programación siempre ha sido una gran parte de ello. He probado C++, OpenGL y la programación de microcontroladores, entre otros, porque soy curioso y me asombra lo que pueden hacer unas simples líneas de código.<br/>
+      Al principio, la programación era solo un pasatiempo para mí, una forma de ser creativo y explorar nuevas tecnologías. Pero a medida que la tecnología avanzaba, decidí convertir mi pasatiempo en una carrera. Comencé a aprender React en enero de 2022 y rápidamente me enamoré de él. Ahora estoy enfocado en convertirme en un desarrollador profesional.<br/>
+      <p>Estoy buscando mi primer trabajo como Desarrollador Frontend. Quiero trabajar con personas que compartan mi pasión por la tecnología y la innovación. Estoy emocionado de unirme a un equipo donde pueda seguir aprendiendo y creciendo como desarrollador.<br/>
+      Si eres una startup que busca crecer, me encantaría unirme y ayudarte a tener éxito con mi trabajo duro y dedicación.</p>
+      <p>Además del desarrollo frontend, también he adquirido algo de experiencia con tecnologías backend. Estoy comprometido con el aprendizaje continuo, y mi objetivo es desarrollar habilidades tanto en frontend como en backend. También quiero ampliar mis conocimientos para incluir el desarrollo de aplicaciones Android e iOS con React.</p>
+      <p>Estoy emocionado por el futuro y lo que me depara.</p>
+      <p>Eso es un poco sobre mí. Ahora, volvamos al tema principal...</p>
+      
+      <p>Mi enfoque principal es usar las tecnologías más recientes para crear excelentes soluciones para los usuarios.</p>
+      <p>* React es la herramienta principal que uso para construir aplicaciones dinámicas de una sola página con navegación fluida y gran rendimiento.<br/>
+      * Next.js agrega renderizado del lado del servidor y generación de sitios estáticos para un mejor rendimiento y SEO.<br/>
+      * TypeScript garantiza que mi código sea seguro y fácil de mantener, haciéndolo confiable para proyectos complejos.<br/>
+      * Tailwind CSS me ayuda a estilizar sitios web rápidamente con un enfoque utility-first.<br/>
+      * Styled-Components ofrece otra forma de estilizar componentes.<br/>
+      * Redux y Axios ayudan a gestionar datos de manera eficiente.</p>
+      <p>Este stack me permite crear aplicaciones web de alto rendimiento, responsivas y con un diseño intuitivo.</p>
+      <p>Un cordial saludo,</p>
+`,
     },
     contact: {
       contactParagraph: "¡Colaboremos para dar vida a las visiones!",
