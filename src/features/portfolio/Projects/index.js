022a56e3eb9e1fa7a7ef32f2c8626c827dc85825
Dariusz Podczasik <boostertech@mail.com>
2025-02-08 21:07:00 +0000

    feat: add TitleWrapper & ProjectsWrapper for styling

--- src/features/portfolio/Projects/index.js
+++ src/features/portfolio/Projects/index.js
@@ -1,4 +1,10 @@
-import { Header, ProjectIcon, Wrapper } from "./styled";
+import {
+  Header,
+  ProjectIcon,
+  ProjectsWrapper,
+  TitleWrapper,
+  Wrapper,
+} from "./styled";
 import projects from "./projects";
 import Tile from "./Tile";
 import gitHubIcon from "../../../images/gitHubIcon.png";
@@ -6,22 +12,26 @@ import gitHubIcon from "../../../images/gitHubIcon.png";
 const Projects = ({ id }) => {
   return (
     <Wrapper id={id}>
-      <ProjectIcon src={gitHubIcon} alt="GitHub Icon" />
-      <Header>Portfolio</Header>
-      {projects.map((project, index) => (
-        <Tile
-          key={index}
-          title={project.title}
-          description={project.description}
-          imageURL={project.imageURL}
-          GitHubPagesURL={project.GitHubPagesURL}
-          GitHubRepoURL={project.GitHubRepoURL}
-          inverted={project.inverted}
-          border={project.border}
-          GitHubPagesURLTag={project.GitHubPagesURLTag}
-          GitHubRepoURLTag={project.GitHubRepoURLTag}
-        />
-      ))}
+      <TitleWrapper>
+        <ProjectIcon src={gitHubIcon} alt="GitHub Icon" />
+        <Header>Projects</Header>
+      </TitleWrapper>
+      <ProjectsWrapper>
+        {projects.map((project, index) => (
+          <Tile
+            key={index}
+            title={project.title}
+            description={project.description}
+            imageURL={project.imageURL}
+            GitHubPagesURL={project.GitHubPagesURL}
+            GitHubRepoURL={project.GitHubRepoURL}
+            inverted={project.inverted}
+            border={project.border}
+            GitHubPagesURLTag={project.GitHubPagesURLTag}
+            GitHubRepoURLTag={project.GitHubRepoURLTag}
+          />
+        ))}
+      </ProjectsWrapper>
     </Wrapper>
   );
 };
