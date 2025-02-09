e84073d155bd8b71ff6912cd13012f878a83f124
Dariusz Podczasik <boostertech@mail.com>
2025-02-09 16:18:04 +0000

    refactor: update aboutMeParagraph text

--- src/themes.js
+++ src/themes.js
@@ -31,15 +31,15 @@ export const themes = {
       aboutMeHeader: "About Me",
       aboutMeParagraph: `
         <p>Hi, I'm Derek,</p> 
-        I've been technology fan since I was a child, and coding has always been a big part of that. I've tried out C++, OpenGL, and MCU programming amongst others because I'm curious and amazed what simple lines of code can do.<br/>
-        At first, coding was just a hobby for me a way to be creative and explore new technologies. But as technology advanced, I decided to turn my hobby into a career. I started learning React in January 2022 and quickly fell in love with it. Now, I'm focused on becoming a professional Developer.<br/>
-        <p>I'm looking for my first job as a Frontend Developer. I want to work with people who share my passion for technology and innovation. I'm excited to join a team where I can continue to learn and grow as a developer.<br/>
-        If you’re a startup company looking to grow, I’d love to join and help you succeed with my hard work and dedication.</p>
-        <p>In addition to frontend development, I've also gained some experience with backend technologies. I'm committed to continuous learning, and my goal is to become skilled in both frontend and backend development. I also want to extend my skills to include Android and iOS development with React.</p>
-        <p>I’m excited about the future and what it holds for me.</p>
-        <p>That’s a bit about me. Now, let’s get back to the main topic...</p>
-        
-        <p>My primary focus is using the latest technologies to create great solutions for users.</p>
+        I've been technology enthusiast since I was a child, and coding has always been a big part of that passion.
+        Over the years, I've worked with C++, OpenGL, and MCU programming amongst others.<br/>
+        Initially, coding was just a hobby for me, an expression of my creativity. As technology evolved, I decided to take it further and walk my path into advancement.<br/> I started learning React in January 2022 and quickly fell in love with it, diving deep into its capabilities..<br/>
+        <p>I'm eager to work with people who share my passion for technology and innovation. I’m excited to join a team where I can contribute my skills and continue to grow as a developer.<br/>
+        If you’re a startup looking to expand, I’d love to help you succeed with my hard work and dedication.</p>
+        <p>I'm committed to continuous learning, with a focus on becoming proficient in both front-end and back-end development.<br/> I also aim to extend my skills to mobile development using React Native for Android and iOS.</p>
+        <p>Looking ahead, I’m excited about the future and the opportunities it holds.</p>
+        <p>Now, a little more about my work:<br/>
+         My primary focus is using the latest technologies to create great solutions for users.</p>
         <p>* React is the main tool I use to build dynamic, single-page applications with smooth navigation and great performance.<br/>
         * Next.js adds server-side rendering and static site generation for better performance and SEO.<br/>
         * TypeScript ensures my code is safe and maintainable, making it reliable for complex projects.<br/>
@@ -71,31 +71,34 @@ export const themes = {
       aboutMeHeader: "O Mnie",
       aboutMeParagraph: `
       <p>Cześć, jestem Derek,</p>
-      Od dziecka byłem fanem technologii, a programowanie zawsze było jej dużą częścią. Próbowałem swoich sił w C++, OpenGL i programowaniu mikrokontrolerów, ponieważ jestem ciekawy i zafascynowany tym, co potrafią zrobić proste linie kodu.<br/>
-      Na początku programowanie było dla mnie tylko hobby – sposobem na bycie kreatywnym i poznawanie nowych technologii. Jednak w miarę jak technologia się rozwijała, postanowiłem   zamienić swoje hobby w karierę. Zacząłem uczyć się Reacta w styczniu 2022 roku i szybko się w nim zakochałem. Teraz skupiam się na zostaniu profesjonalnym deweloperem.<br/>
-      <p>Szukam swojej pierwszej pracy jako Frontend Developer. Chcę pracować z ludźmi, którzy podzielają moją pasję do technologii i innowacji. Jestem podekscytowany możliwością    dołączenia do zespołu, w którym będę mógł się uczyć i rozwijać jako programista.<br/>
-      Jeśli jesteś startupem, który chce się rozwijać, chętnie dołączę i pomogę osiągnąć sukces moją ciężką pracą i zaangażowaniem.</p>
-      <p>Oprócz programowania frontendowego zdobyłem również trochę doświadczenia w technologiach backendowych. Jestem zaangażowany w ciągłe doskonalenie i moim celem jest zdobycie   umiejętności zarówno w frontendzie, jak i backendzie. Chcę również poszerzyć swoje umiejętności o rozwój aplikacji na Androida i iOS za pomocą Reacta.</p>
-      <p>Jestem podekscytowany przyszłością i tym, co mnie czeka.</p>
-      <p>To tyle o mnie. Teraz wróćmy do głównego tematu...</p>
-      
-      <p>Moim głównym celem jest wykorzystywanie najnowszych technologii do tworzenia świetnych rozwiązań dla użytkowników.</p>
-      <p>* React to główne narzędzie, które wykorzystuję do budowy dynamicznych aplikacji jednostronicowych z płynną nawigacją i świetną wydajnością.<br/>
-      * Next.js dodaje renderowanie po stronie serwera i generowanie statycznych witryn dla lepszej wydajności i SEO.<br/>
-      * TypeScript zapewnia bezpieczeństwo i łatwość utrzymania mojego kodu, co czyni go niezawodnym w przypadku złożonych projektów.<br/>
-      * Tailwind CSS pomaga mi szybko stylizować witryny dzięki podejściu "utility-first".<br/>
-      * Styled-Components oferuje alternatywny sposób stylizacji komponentów.<br/>
-      * Redux i Axios pomagają efektywnie zarządzać danymi.</p>
-      <p>Ten stos technologiczny pozwala mi budować wydajne, responsywne aplikacje internetowe o intuicyjnym designie.</p>
-      <p>Serdeczne pozdrowienia,</p>
+<p>Od dziecka pasjonuję się technologią, a programowanie zawsze było dla mnie czymś szczególnym. Przez lata miałem okazję pracować z C++, OpenGL i programowaniem MCU, a to tylko niektóre z technologii, które poznałem.</p>
+<p>Na początku programowanie traktowałem jako hobby — sposób na wyrażenie swojej kreatywności. Z biegiem czasu, w miarę jak technologia się rozwijała, postanowiłem, że to nie tylko zabawa, ale także droga, którą chcę podążać zawodowo.</p>
+<p>W styczniu 2022 roku zacząłem naukę Reacta i od razu poczułem, że to narzędzie, które mogę naprawdę zgłębić. Zafascynowałem się jego możliwościami i nie przestałem się uczyć.</p>
+<p>Chciałbym pracować z ludźmi, którzy podzielają moją pasję do technologii i innowacji. Cieszę się na myśl o dołączeniu do zespołu, w którym mogę wykorzystać swoje umiejętności i jednocześnie się rozwijać jako programista.</p>
+<p>Jeśli jesteś startupem, który szuka ludzi z zaangażowaniem, chętnie pomogę Ci osiągnąć sukces swoją ciężką pracą i oddaniem.</p>
+
+<p>Stawiam na ciągły rozwój, zarówno w zakresie front-endu, jak i back-endu. Chciałbym także poszerzyć swoje umiejętności o aplikacje mobilne, korzystając z React Native na Androida i iOS.</p>
+<p>Patrzę w przyszłość z ekscytacją i nie mogę się doczekać, co ona przyniesie.</p>
+<p>A teraz, kilka słów o mojej pracy:<br/>
+Moim celem jest wykorzystywanie najnowszych technologii do tworzenia rozwiązań, które naprawdę służą użytkownikom.</p>
+<p>* React to narzędzie, którego używam do budowania dynamicznych, jednokolumnowych aplikacji z płynna nawigacją i świetną wydajnością.<br/>
+* Next.js umożliwia renderowanie po stronie serwera i generowanie statycznych stron, co poprawia wydajność i SEO.<br/>
+* TypeScript zapewnia bezpieczeństwo i łatwość utrzymania mojego kodu, dzięki czemu jest niezawodny w bardziej złożonych projektach.<br/>
+* Tailwind CSS pomaga mi szybko stylizować strony w sposób elastyczny, oparty na podejściu „utility-first”.<br/>
+* Styled-Components to alternatywna metoda stylizacji komponentów, która również znajduje swoje miejsce w mojej pracy.<br/>
+* Redux i Axios są moimi sprzymierzeńcami w efektywnym zarządzaniu danymi.</p>
+
+<p>Dzięki tej technologii tworzę aplikacje internetowe o wysokiej wydajności, responsywne i z intuicyjnym designem.</p>
+<p>Pozdrawiam,</p>
+
+
 `,
     },
     contact: {
       contactParagraph: "Współpracujmy, aby urzeczywistnić wizje!",
     },
     footer: {
-      footerParagraph:
-        "Copyright© 2024 Dariusz Podczasik",
+      footerParagraph: "Copyright© 2024 Dariusz Podczasik",
     },
   },
 
@@ -111,23 +114,27 @@ export const themes = {
       aboutMeHeader: "Sobre Mí",
       aboutMeParagraph: `
       <p>Hola, soy Derek,</p>
-      Desde niño he sido un fanático de la tecnología, y la programación siempre ha sido una gran parte de ello. He probado C++, OpenGL y la programación de microcontroladores, entre otros, porque soy curioso y me asombra lo que pueden hacer unas simples líneas de código.<br/>
-      Al principio, la programación era solo un pasatiempo para mí, una forma de ser creativo y explorar nuevas tecnologías. Pero a medida que la tecnología avanzaba, decidí convertir mi pasatiempo en una carrera. Comencé a aprender React en enero de 2022 y rápidamente me enamoré de él. Ahora estoy enfocado en convertirme en un desarrollador profesional.<br/>
-      <p>Estoy buscando mi primer trabajo como Desarrollador Frontend. Quiero trabajar con personas que compartan mi pasión por la tecnología y la innovación. Estoy emocionado de unirme a un equipo donde pueda seguir aprendiendo y creciendo como desarrollador.<br/>
-      Si eres una startup que busca crecer, me encantaría unirme y ayudarte a tener éxito con mi trabajo duro y dedicación.</p>
-      <p>Además del desarrollo frontend, también he adquirido algo de experiencia con tecnologías backend. Estoy comprometido con el aprendizaje continuo, y mi objetivo es desarrollar habilidades tanto en frontend como en backend. También quiero ampliar mis conocimientos para incluir el desarrollo de aplicaciones Android e iOS con React.</p>
-      <p>Estoy emocionado por el futuro y lo que me depara.</p>
-      <p>Eso es un poco sobre mí. Ahora, volvamos al tema principal...</p>
-      
-      <p>Mi enfoque principal es usar las tecnologías más recientes para crear excelentes soluciones para los usuarios.</p>
-      <p>* React es la herramienta principal que uso para construir aplicaciones dinámicas de una sola página con navegación fluida y gran rendimiento.<br/>
-      * Next.js agrega renderizado del lado del servidor y generación de sitios estáticos para un mejor rendimiento y SEO.<br/>
-      * TypeScript garantiza que mi código sea seguro y fácil de mantener, haciéndolo confiable para proyectos complejos.<br/>
-      * Tailwind CSS me ayuda a estilizar sitios web rápidamente con un enfoque utility-first.<br/>
-      * Styled-Components ofrece otra forma de estilizar componentes.<br/>
-      * Redux y Axios ayudan a gestionar datos de manera eficiente.</p>
-      <p>Este stack me permite crear aplicaciones web de alto rendimiento, responsivas y con un diseño intuitivo.</p>
-      <p>Un cordial saludo,</p>
+<p>Desde que era niño, la tecnología siempre ha sido una de mis grandes pasiones, y la programación ha sido una parte esencial de eso. A lo largo de los años, he trabajado con C++, OpenGL y programación en MCU, entre otras tecnologías.</p>
+<p>Al principio, la programación era solo un hobby para mí, una forma de dar rienda suelta a mi creatividad. Pero a medida que la tecnología avanzaba, decidí llevarlo más lejos y hacer de esto mi carrera.</p>
+<p>Comencé a aprender React en enero de 2022, y rápidamente me enamoré de él, explorando todas sus posibilidades.</p>
+<p>Me encantaría trabajar con personas que compartan mi pasión por la tecnología y la innovación. Estoy emocionado de unirme a un equipo donde pueda aportar mis habilidades y seguir creciendo como desarrollador.</p>
+<p>Si eres una startup que está buscando crecer, me encantaría ayudarte a alcanzar el éxito con mi esfuerzo y dedicación.</p>
+
+<p>Estoy totalmente comprometido con el aprendizaje continuo, mejorando tanto mis habilidades en el desarrollo frontend como backend. Además, quiero expandir mis conocimientos al desarrollo móvil con React Native para Android e iOS.</p>
+<p>Viendo hacia el futuro, me siento entusiasmado por las oportunidades que nos esperan.</p>
+<p>Y ahora, un poco más sobre lo que hago:<br/>
+Mi enfoque principal es usar las últimas tecnologías para crear soluciones de calidad para los usuarios.</p>
+<p>* React es la herramienta que uso para crear aplicaciones de una sola página, con navegación fluida y un rendimiento excepcional.<br/>
+* Next.js permite el renderizado del lado del servidor y la generación de sitios estáticos para mejorar tanto el rendimiento como el SEO.<br/>
+* TypeScript me asegura que mi código sea seguro y fácil de mantener, lo que lo hace ideal para proyectos más complejos.<br/>
+* Tailwind CSS me permite estilizar rápidamente sitios web con un enfoque utility-first.<br/>
+* Styled-Components es otra forma eficiente de estilizar componentes en React.<br/>
+* Redux y Axios son mis aliados para gestionar datos de manera eficiente.</p>
+
+<p>Con esta tecnología, puedo crear aplicaciones web de alto rendimiento, con un diseño intuitivo y responsive.</p>
+<p>Saludos,</p>
+
+
 `,
     },
     contact: {
