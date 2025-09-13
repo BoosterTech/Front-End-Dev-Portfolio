7ef134ab014d8fb311a224b7b7adf81c3090d920
Dariusz Podczasik <boostertech@mail.com>
2025-09-13 19:13:25 +0100

    Update Tile: alternate image/description order for correct grid layout

--- src/features/portfolio/Projects/Tile/index.js
+++ src/features/portfolio/Projects/Tile/index.js
@@ -16,36 +16,62 @@ const Tile = ({
   imageURL,
   GitHubPagesURL,
   GitHubRepoURL,
-  inverted,
   border = true,
   GitHubPagesURLTag,
   GitHubRepoURLTag,
+  index,
 }) => {
   const language = useSelector(selectLanguage);
 
   return (
-    <ProjectWrapper $border={border} $inverted={inverted}>
-      <ProjectImage 
-        src={imageURL} 
-        alt={`${title[language]} project screenshot`} 
-        $inverted={inverted}
-      />
-      <ProjectDescription $inverted={inverted}>
-        <ProjectHeader>{title[language]}</ProjectHeader>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: description[language],
-          }}
-        />
-        <LinkContainer>
-          <ProjectLink href={GitHubPagesURL} target="_blank" rel="noopener noreferrer">
-            <LinkTag>{GitHubPagesURLTag[language]}</LinkTag>
-          </ProjectLink>
-          <ProjectLink href={GitHubRepoURL} target="_blank" rel="noopener noreferrer">
-            <LinkTag>{GitHubRepoURLTag[language]}</LinkTag>
-          </ProjectLink>
-        </LinkContainer>
-      </ProjectDescription>
+    <ProjectWrapper $border={border}>
+      {index % 2 === 0 ? (
+        <>
+          <ProjectImage 
+            src={imageURL} 
+            alt={`${title[language]} project screenshot`} 
+          />
+          <ProjectDescription>
+            <ProjectHeader>{title[language]}</ProjectHeader>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: description[language],
+              }}
+            />
+            <LinkContainer>
+              <ProjectLink href={GitHubPagesURL} target="_blank" rel="noopener noreferrer">
+                <LinkTag>{GitHubPagesURLTag[language]}</LinkTag>
+              </ProjectLink>
+              <ProjectLink href={GitHubRepoURL} target="_blank" rel="noopener noreferrer">
+                <LinkTag>{GitHubRepoURLTag[language]}</LinkTag>
+              </ProjectLink>
+            </LinkContainer>
+          </ProjectDescription>
+        </>
+      ) : (
+        <>
+          <ProjectDescription>
+            <ProjectHeader>{title[language]}</ProjectHeader>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: description[language],
+              }}
+            />
+            <LinkContainer>
+              <ProjectLink href={GitHubPagesURL} target="_blank" rel="noopener noreferrer">
+                <LinkTag>{GitHubPagesURLTag[language]}</LinkTag>
+              </ProjectLink>
+              <ProjectLink href={GitHubRepoURL} target="_blank" rel="noopener noreferrer">
+                <LinkTag>{GitHubRepoURLTag[language]}</LinkTag>
+              </ProjectLink>
+            </LinkContainer>
+          </ProjectDescription>
+          <ProjectImage 
+            src={imageURL} 
+            alt={`${title[language]} project screenshot`} 
+          />
+        </>
+      )}
     </ProjectWrapper>
   );
 };
